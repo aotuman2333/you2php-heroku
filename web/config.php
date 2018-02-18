6f93ca1f017bd9fbee97c861ac15c6fccab911a4
aotuman2333 <aotuman233@gmail.com>
2018-02-18 21:05:22 +0800

    Update config.php

--- web/config.php
+++ web/config.php
@@ -1,9 +1,9 @@
 <?php
 define('ROOT_PART', Root_part());
-define('APIKEY', '*********************************');//这里改成您的api key
-define('GJ_CODE', 'KR');//国家代码
-define('SITE_NAME', 'Youtube');//网站名字
-define('TITLENAME', 'Youtube');//网站名字
+define('APIKEY', 'AIzaSyA78kem0kRI1Ge4IM3lWCVrtAtiJk2q9Uo');//这里改成您的api key
+define('GJ_CODE', 'HK');//国家代码
+define('SITE_NAME', 'Y2B');//网站名字
+define('TITLENAME', 'y2b');//网站名字
 define('EN2DEKEY', 'AnQUP1XDEF435e67CtzDSh4');//一个随机字符串，加解密KEY
-define('EMAIL', '123@123.com');//邮箱
+define('EMAIL', 'aotuman233@gmail.com');//邮箱
 ?>
